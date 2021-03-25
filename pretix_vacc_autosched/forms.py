00895b57f828c84c91023607b1e0e9b67160a1e8
Tobias Kunze <r@rixx.de>
2021-03-25 13:39:03 +0100

    Move AutoschedSettingsForm to forms

--- pretix_vacc_autosched/forms.py
+++ pretix_vacc_autosched/forms.py
@@ -2,6 +2,9 @@ from django import forms
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 from django_scopes.forms import SafeModelChoiceField
+from i18nfield.forms import I18nFormField, I18nTextarea, I18nTextInput
+from pretix.base.email import get_available_placeholders
+from pretix.base.forms import PlaceholderValidator, SettingsForm
 
 from .models import ItemConfig
 
@@ -60,3 +63,47 @@ class ItemConfigForm(forms.ModelForm):
                 return
         else:
             return super().save(commit=commit)
+
+
+class AutoschedSettingsForm(SettingsForm):
+    vacc_autosched_mail = forms.BooleanField(
+        label=_("Send email if second dose has been scheduled"),
+        required=False,
+    )
+
+    vacc_autosched_subject = I18nFormField(
+        label=_("Email Subject"),
+        required=True,
+        widget=I18nTextInput,
+    )
+
+    vacc_autosched_body = I18nFormField(
+        label=_("Email Body"),
+        required=True,
+        widget=I18nTextarea,
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.event = kwargs.pop("obj")
+
+        self._set_field_placeholders(
+            "vacc_autosched_subject", ["event"], ["{scheduled_datetime}"]
+        )
+        self._set_field_placeholders(
+            "vacc_autosched_body", ["event"], ["{scheduled_datetime}"]
+        )
+
+    def _set_field_placeholders(self, fn, base_parameters, extras=[]):
+        phs = [
+            "{%s}" % p
+            for p in sorted(
+                get_available_placeholders(self.event, base_parameters).keys()
+            )
+        ] + extras
+        ht = _("Available placeholders: {list}").format(list=", ".join(phs))
+        if self.fields[fn].help_text:
+            self.fields[fn].help_text += " " + str(ht)
+        else:
+            self.fields[fn].help_text = ht
+        self.fields[fn].validators.append(PlaceholderValidator(phs))
